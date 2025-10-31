b0c10863f0df54b202510e21aab4bfb19d508df2
chuang wang <18232465472@163.com>
2025-10-31 20:42:49 +1030

    123

--- src/main/java/com/yindi/card/user/upgrade/UpgradeController.java
+++ src/main/java/com/yindi/card/user/upgrade/UpgradeController.java
@@ -29,7 +29,8 @@ public class UpgradeController {
     public ResponseEntity<?> uploadJsonPart(
             @PathVariable Long userId,
             @RequestPart("request") String requestJson,
-            @RequestPart(value="file", required=false) MultipartFile file
+            @RequestPart(value="file", required=false) Multip
+                    artFile file
     ) throws Exception {
         UpgradeRequest req = objectMapper.readValue(requestJson, UpgradeRequest.class);
         save(userId, req, file);  // 复用保存逻辑
